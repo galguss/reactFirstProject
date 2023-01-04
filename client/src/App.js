092cc006171a80e1f608b39bf997e31b04f95e9d
galguss <gal.gus12@gmail.com>
2023-01-04 11:14:08 +0200

    A collection of components Container and Input

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import Container from "./components/Container"
+
 function App() {
   return (
     <div className="App">
-      <h1>hellow world!</h1>
+      <Container/>
     </div>
   );
 }
---
+++ client/src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Input from './Input';
+
+function Container(){
+    return (
+        <>
+            <Input/>
+        </>
+    )
+}
+
+export default Container;
